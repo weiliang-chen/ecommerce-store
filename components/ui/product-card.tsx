162e36953d9328c4892cb98b751b3c3eb76b26ca
Weiliang Chen <liang2dd@gmail.com>
2023-11-18 17:32:57 +1100

    implement product detail page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -5,13 +5,23 @@ import Image from "next/image";
 import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingBag } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
   data: Product;
 }
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
+
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       {/* image */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
@@ -33,19 +43,15 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
           </div>
         </div>
       </div>
-       {/* desc */}
-       <div>
-        <p className="font-semibold text-lg">
-            {data.name}
-        </p>
-        <p className="tedt- sm text-grey-500">
-            {data.category?.name}
-        </p>
-       </div>
-       {/* price */}
-       <div className="flex items-center justify-between">
-            <Currency value = {data?.price} />
-       </div>
+      {/* desc */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="tedt- sm text-grey-500">{data.category?.name}</p>
+      </div>
+      {/* price */}
+      <div className="flex items-center justify-between">
+        <Currency value={data?.price} />
+      </div>
     </div>
   );
 };
