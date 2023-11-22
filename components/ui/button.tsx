94b5111089597a096228b5a74cb2c3389f7d2f92
Weiliang Chen <liang2dd@gmail.com>
2023-11-22 16:15:51 +1100

    fix button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,8 +9,10 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <button
         className={cn(
-          `w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`, className
+          `w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`,
+          className
         )}
+        disabled={disabled}
         ref={ref}
         {...props}
       >
